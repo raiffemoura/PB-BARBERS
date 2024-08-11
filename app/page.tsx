cf7118872097c8c8caa826fa41e6149e4c05f2d9
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2024-08-11 16:05:25 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@ import Header from "./_components/ui/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
     <>
       <Header />
       <div className="p-5">
+        {/* TEXTO */}
         <h2 className="text-xl font-bold">Olá, Raiffe!</h2>
         <p>Domingo, 11 de agosto </p>
+        {/* BUSCA */}
 
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..."></Input>
@@ -18,7 +23,7 @@ const Home = () => {
             <SearchIcon></SearchIcon>
           </Button>
         </div>
-        {/* BANNER */}
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner-01.png"
@@ -27,6 +32,29 @@ const Home = () => {
             fill
           />
         </div>
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* div da esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/23d9c4f7-8bdb-40e1-99a5-f42271b7404a-17q.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* div da direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">10</p>
+              <p className="text-sm">11:30</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   )
