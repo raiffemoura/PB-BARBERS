6fbad7af8642ad2a1fa25caf3754079194e544ad
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2024-08-14 10:58:49 -0300

    feat: add sidebar

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -1,16 +1,90 @@
 import Image from "next/image"
 import { Card, CardContent } from "./card"
-import { MenuIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
 import { Button } from "./button"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./sheet"
+import { quickSearchOptions } from "@/app/_constants/search"
+import { Avatar, AvatarImage } from "./avatar"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Image src="/logo.png" alt="PB Barbers" width={120} height={18} />
-        <Button size="icon" variant="outline">
-          <MenuIcon></MenuIcon>
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon></MenuIcon>
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
+
+            <div className="flex items-center gap-3 border-b border-solid py-5">
+              <Avatar>
+                <AvatarImage src="https://img.freepik.com/fotos-gratis/homem-retrato-rindo_23-2148859448.jpg?w=740&t=st=1723643614~exp=1723644214~hmac=052568b7a7e2bb71db492b7a3e18e117f051d77dabe32e58b16b587300484215" />
+              </Avatar>
+              <div>
+                <p className="font-bold">Raiffe Moura</p>
+                <p className="text-xs">raiffe@google.com</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              <SheetClose asChild>
+                <Button
+                  className="justify-start gap-2"
+                  variant={"ghost"}
+                  asChild
+                >
+                  <Link href={"/"}>
+                    <HomeIcon size={18} />
+                    Início
+                  </Link>
+                </Button>
+              </SheetClose>
+              <Button className="justify-start gap-2" variant={"ghost"}>
+                <CalendarIcon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  className="justify-start gap-2"
+                  variant={"ghost"}
+                  key={option.title}
+                >
+                  <Image
+                    src={option.imageUrl}
+                    alt={option.title}
+                    width={18}
+                    height={18}
+                  />
+                  {option.title}
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 py-5">
+              <Button className="justify-start gap-2" variant={"ghost"}>
+                <LogOutIcon size={18} />
+                Sair da conta
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   )
