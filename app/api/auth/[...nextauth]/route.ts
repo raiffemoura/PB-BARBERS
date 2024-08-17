4fd630231165af630461e985c1f6ed7f6d80868f
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2024-08-17 17:13:41 -0300

    feat: ensure only authenticated user can book

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,7 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { db } from "@/app/_lib/prisma"
-import { Adapter } from "next-auth/adapters"
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
+import { authOptions } from "@/app/_lib/auth"
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-
-  callbacks: {
-    async session({ session, user }) {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: user.id,
-        },
-      }
-    },
-  },
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
