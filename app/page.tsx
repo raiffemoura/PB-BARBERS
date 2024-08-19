f38057d1c82e3026c1740de12f23920804a6eb67
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2024-08-19 13:43:51 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -46,8 +48,18 @@ const Home = async () => {
       <div className="p-5">
         {/* TEXTO */}
 
-        <h2 className="text-xl font-bold">Olá, Raiffe!</h2>
-        <p>Domingo, 11 de agosto </p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user?.name ? session?.user?.name : "bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
         {/* BUSCA */}
         <div className="mt-6">
           <Search />
